4c39fc1642f3c7fc8a9aa89aa2357874e4febe91
Joaquin Martinez <joaco1509@gmail.com>
2019-10-03 19:49:44 -0300

    Agregue algunas cosas que todavia no funcionan

---
+++ tic1/src/main/java/um/edu/tic1/controllers/InicioController.java
@@ -0,0 +1,34 @@
+package um.edu.tic1.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
+import um.edu.tic1.Tic1Application;
+
+import java.io.IOException;
+
+@Component
+public class InicioController {
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    public void adminEmpresa(ActionEvent actionEvent)throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setControllerFactory(Tic1Application.getContext()::getBean);
+
+        Parent inicio = fxmlLoader.load(getClass().getResourceAsStream("/templates/mostrar.fxml"));
+        Scene inicioScene = new Scene(inicio);
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        window.setScene(inicioScene);
+        window.show();
+    }
+}
---
+++ tic1/src/main/java/um/edu/tic1/controllers/ViewFilmsController.java
@@ -0,0 +1,83 @@
+package um.edu.tic1.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import org.springframework.beans.factory.annotation.Autowired;
+import um.edu.tic1.entities.Movie;
+import um.edu.tic1.services.MovieService;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+
+
+public class ViewFilmsController implements Initializable {
+
+    HBox hb = new HBox();
+    
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private GridPane grid;
+
+    @Autowired
+    private MovieService ms;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+
+            ObservableList<Movie> movies = getMovie();
+
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+
+            // gridpane settings
+            // setting exterior grid padding
+            grid.setPadding(new Insets(7,7,7,7));
+            // setting interior grid padding
+            grid.setHgap(10);
+            grid.setVgap(10);
+            // grid.setGridLinesVisible(true);
+
+            int rows = (movies.size() / 4) + 1;
+            int columns = 4;
+            int indicePeli = 0;
+
+            for (int i = 0 ; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    if (indicePeli < movies.size()) {
+                        grid.a
+                        indicePeli++;
+                    }
+                }
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }}
+
+    private ObservableList<Movie> getMovie() {
+
+        ObservableList<Movie> movie = FXCollections.observableArrayList();
+
+        List<Movie> lista = ms.findAll();
+
+        for (int i = 0; i < lista.size(); i++) {
+            movie.add(lista.get(i));
+        }
+
+        return movie;
+    }
+
+}
