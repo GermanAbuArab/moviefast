54c571e0ed44f93a19e605f60963717cdab748ce
Joaquin Martinez <joaco1509@gmail.com>
2019-10-26 10:11:07 -0300

    Agregada funcion en login

---
+++ tic1/src/main/java/um/edu/tic1/controllers/AdentroCineController.java
@@ -0,0 +1,64 @@
+package um.edu.tic1.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdentroCineController {
+
+    @FXML
+    private Button botonAplicar;
+
+    @FXML
+    private Button peliculas;
+
+    @FXML
+    private TableView<?> tabla;
+
+    @FXML
+    private TableColumn<?, ?> nombrePeli;
+
+    @FXML
+    private TableColumn<?, ?> descripcion;
+
+    @FXML
+    private TableColumn<?, ?> categoria;
+
+    @FXML
+    private TableColumn<?, ?> genero;
+
+    @FXML
+    void agregar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editNombreCommit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void entrar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void peliculas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void volver(ActionEvent event) {
+
+    }
+
+}
--- tic1/src/main/java/um/edu/tic1/controllers/loginController.java
+++ tic1/src/main/java/um/edu/tic1/controllers/loginController.java
@@ -79,6 +79,17 @@ public class loginController implements Initializable {
 
         }
 
+        if(cineEncontrado){
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setControllerFactory(Tic1Application.getContext()::getBean);
+            Parent inicio = fxmlLoader.load(getClass().getResourceAsStream("/templates/adentroCine.fxml"));
+            Scene inicioScene = new Scene(inicio, 600, 500);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(inicioScene);
+            window.show();
+
+        }
+
         if(!cineEncontrado){
             FXMLLoader fxmlLoader = new FXMLLoader();
             fxmlLoader.setControllerFactory(Tic1Application.getContext()::getBean);
@@ -87,7 +98,6 @@ public class loginController implements Initializable {
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
             window.setScene(inicioScene);
             window.show();
-            System.out.println("NOTIFICADO" +user);
         }
     }
 }
