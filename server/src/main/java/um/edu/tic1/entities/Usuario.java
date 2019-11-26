15eeca8717eeb98212921ce5ce5f91ba6f200717
jmartinez5 <joaco1509@gmail.com>
2019-11-26 10:44:43 -0300

    FindById

--- server/src/main/java/um/edu/tic1/entities/Usuario.java
+++ server/src/main/java/um/edu/tic1/entities/Usuario.java
@@ -7,8 +7,7 @@ import javax.persistence.*;
 @Entity
 @Table(name="usuario")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type",
-        discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
 public abstract class Usuario {
 
     @Id
